95b6fab9e375ae152606359bde0ff94384cbc7cf
SAHA-YJB <pkomom123@naver.com>
2023-12-13 10:34:10 +0900

    fetures: router-dom / tanstackquery setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 function App() {
-  return <div></div>;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path='*' element={<Home />} />
+        <Route path='/:id' element={<Home />} />
+      </Routes>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
